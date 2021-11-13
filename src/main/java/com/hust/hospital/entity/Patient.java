cf55a6af157920cd4745a30b8a4763b0ac5162b0
1768001587 <1768001587@qq.com>
2021-11-13 21:01:43 +0800

    '智慧医疗后台‘

---
+++ src/main/java/com/hust/hospital/entity/Patient.java
@@ -0,0 +1,41 @@
+package com.hust.hospital.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author EdwardXu
+ * @description 病人
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Patient {
+
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 床号
+     */
+    private Integer bed;
+
+    /**
+     * 状态
+     */
+    private Integer status;
+
+    public Patient(String name, Integer bed, Integer status) {
+        this.name = name;
+        this.bed = bed;
+        this.status = status;
+    }
+}
